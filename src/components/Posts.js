a78340bb44d98f32110cbed7c0235558fb23989a
Quy Dinh <quy.dinh3195@gmail.com>
2024-05-08 15:07:32 +0700

    PL-1 Chapter 6 Automatic scrolling down in place of More button

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Spinner } from "react-bootstrap";
 import Post from "./Post";
 import More from "./More";
@@ -7,17 +7,19 @@ import { useApi } from "../contexts/ApiProvider";
 export default function Posts({ content }) {
   const [posts, setPosts] = useState();
   const [pagination, setPagination] = useState();
+  const [loading, setLoading] = useState(false);
   const api = useApi();
+  const moreRef = useRef();
 
   let url;
   switch (content) {
-    case 'feed':
+    case "feed":
     case undefined:
-      url = '/feed';
+      url = "/feed";
       break;
-    case 'explore':
-      url = '/posts';
-      break
+    case "explore":
+      url = "/posts";
+      break;
     default:
       url = `/users/${content}/posts`;
       break;
@@ -28,46 +30,76 @@ export default function Posts({ content }) {
       const results = await api.get(url);
       if (results.ok) {
         setPosts(results.body.data);
-        setPagination(results.body.pagination)
-      }
-      else {
+        setPagination(results.body.pagination);
+      } else {
         setPosts(null);
       }
     })();
-  // A simple rule to remember, is that when this argument is set to an empty array, the side effect function runs once when the component is first rendered and never again.
-  // A common mistake is to forget to include the second argument. This is interpreted by React as instructions to run the side effect function every single time the component renders, which is rarely necessary.
+    // A simple rule to remember, is that when this argument is set to an empty array, the side effect function runs once when the component is first rendered and never again.
+    // A common mistake is to forget to include the second argument. This is interpreted by React as instructions to run the side effect function every single time the component renders, which is rarely necessary.
   }, [api, url]);
 
   const loadNextPage = async () => {
+    if (loading || pagination?.offset === undefined) return;
+    setLoading(true);
     const response = await api.get(url, {
-      after: posts[posts.length - 1].timestamp
+      offset: pagination.offset + pagination.count,
     });
     if (response.ok) {
       setPosts([...posts, ...response.body.data]);
       setPagination(response.body.pagination);
     }
+    setLoading(false);
   };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        // console.log(`entries[0].isIntersecting=${entries[0].isIntersecting}`)
+        if (entries[0].isIntersecting) {
+          loadNextPage();
+        }
+      },
+      { threshold: 1.0 } // Trigger only when the `More` button is fully visible
+    );
+
+    if (moreRef.current) {
+      observer.observe(moreRef.current);
+    }
+
+    return () => {
+      if (moreRef.current) {
+        observer.unobserve(moreRef.current);
+      }
+    };
+  }, [moreRef.current, posts, pagination]);
 
   return (
     <>
-      {posts === undefined ?
-        <Spinner animation="border"/>
-      :
+      {posts === undefined ? (
+        <Spinner animation="border" />
+      ) : (
         <>
-          {posts === null ?
+          {posts === null ? (
             <p>Failed to load posts</p>
-          :
+          ) : (
             <>
-              {posts.length === 0 ?
+              {posts.length === 0 ? (
                 <p>There are no blog posts.</p>
-              :
-                posts.map(post => <Post key={post.id} post={post} />)
-              }
-              <More pagination={pagination} loadNextPage={loadNextPage} />
+              ) : (
+                posts.map((post) => <Post key={post.id} post={post} />)
+              )}
+              <div ref={moreRef}>
+                {loading ? (
+                  <Spinner animation="border" />
+                ) : (
+                  <More pagination={pagination} loadNextPage={loadNextPage} />
+                )}
+              </div>
             </>
-          }
+          )}
         </>
-      }
+      )}
     </>
   );
 }
