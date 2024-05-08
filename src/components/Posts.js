6ec6afb4a5b5d3a8bf7a1b5eae9a5e6c0e88c40f
Quy Dinh <quy.dinh3195@gmail.com>
2024-05-08 11:49:54 +0700

    PL-1 Chapter 6 Add API Provider context hook and implement Explore and User pages

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,18 +1,31 @@
 import { useState, useEffect } from "react";
 import { Spinner } from "react-bootstrap";
 import Post from "./Post";
+import { useApi } from "../contexts/ApiProvider";
 
-const BASE_API_URL = process.env.REACT_APP_BASE_API_URL;
-
-export default function Posts() {
+export default function Posts({ content }) {
   const [posts, setPosts] = useState();
+  const api = useApi();
+
+  let url;
+  switch (content) {
+    case 'feed':
+    case undefined:
+      url = '/feed';
+      break;
+    case 'explore':
+      url = '/posts';
+      break
+    default:
+      url = `/users/${content}/posts`;
+      break;
+  }
 
   useEffect(() => {
     (async () => {
-      const response = await fetch(BASE_API_URL + '/api/feed');
-      if (response.ok) {
-        const results = await response.json();
-        setPosts(results.data);
+      const results = await api.get(url);
+      if (results.ok) {
+        setPosts(results.body.data);
       }
       else {
         setPosts(null);
@@ -20,7 +33,7 @@ export default function Posts() {
     })();
   // A simple rule to remember, is that when this argument is set to an empty array, the side effect function runs once when the component is first rendered and never again.
   // A common mistake is to forget to include the second argument. This is interpreted by React as instructions to run the side effect function every single time the component renders, which is rarely necessary.
-  }, []);
+  }, [api, url]);
 
   return (
     <>
--- src/pages/ExplorePage.js
+++ src/pages/ExplorePage.js
@@ -1,10 +1,12 @@
 import Body from '../components/Body';
+import Posts from '../components/Posts';
 
 export default function ExplorePage() {
   return (
     <Body sidebar>
       <h1>Explore</h1>
-      <p>TODO</p>
+      <hr />
+      <Posts content="explore" />
     </Body>
   );
 }
